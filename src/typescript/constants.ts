96a9952d4fe18e7904e216b06aa746c44d7ab0d5
Daniel Reyes <danielsoftwaredev@gmail.com>
2024-05-13 18:56:55 -0600

    UI added

---
+++ src/typescript/constants.ts
@@ -0,0 +1,176 @@
+export const menuLinks = [
+    {
+        name: "About",
+        href: "/",
+    },
+    {
+        name: "Store",
+        href: "/store",
+    },
+    {
+        name: "Portfolio",
+        href: "/portfolio",
+    },
+]
+
+export const socialLinks = [
+    //{
+    //    icon: "social/linkedin",
+    //    name: "Linkedin",
+    //    href: "https://www.linkedin.com/in/daniel-reyes-579870303/?trk=opento_sprofile_topcard",
+    //},
+    {
+        icon: "social/github",
+        name: "Github",
+        href: "https://github.com/DanielSoftwareDeveloper?tab=repositories",
+    },
+    {
+        icon: "social/whatsapp",
+        name: "Whatsapp",
+        href: "https://github.com/DanielSoftwareDeveloper?tab=repositories",
+    },
+]
+
+//PRODUCTS
+export const productList = [
+    {
+        image: "https://images.unsplash.com/photo-1614194076674-0598e6dd5703?q=80&w=1971&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        name: "Portfolio template",
+        desc: "A beautiful portfolio for show your projects.",
+        price: 500,
+    },
+    {
+        image: "https://images.unsplash.com/photo-1581235720704-06d3acfcb36f?q=80&w=2080&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        name: "Ecommerce Template",
+        desc: "Ecommerce Template with admin system",
+        price: 1500,
+    },
+    {
+        image: "https://images.unsplash.com/photo-1572635196184-84e35138cf62?q=80&w=2080&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        name: "Real State Template",
+        desc: "Real state template system for sell houses and more.",
+        price: 2000,
+    },
+    {
+        image: "https://images.unsplash.com/photo-1515955656352-a1fa3ffcd111?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        name: "Hotel Managment",
+        desc: "Hotel managment rooms system for avalivility",
+        price: 1500,
+    },
+    {
+        image: "https://images.unsplash.com/photo-1542291026-7eec264c27ff?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        name: "Condominium Management",
+        desc: "Condominium management system for offices and depertments administration.",
+        price: 1500,
+    },
+]
+
+//PORTFOLIO
+export const portfolioProjects = [
+    {
+        image: "https://plus.unsplash.com/premium_photo-1667238640931-4cb8687fa0f3?q=80&w=1927&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        name: "Ecommerce App",
+        href: "https://next-ecommerce-aplication.vercel.app/",
+    },
+    {
+        image: "https://images.unsplash.com/photo-1613376023733-0a73315d9b06?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        name: "Anime Streaming App",
+        href: "",
+    },
+    {
+        image: "https://images.unsplash.com/photo-1601972602237-8c79241e468b?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        name: "Movil App",
+        href: "",
+    },
+    {
+        image: "https://images.unsplash.com/photo-1632312527375-bd5d5a0d3484?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        name: "Videogame",
+        href: "",
+    },
+]
+
+//ARTICLES
+export const articleList = [
+    {
+        image: "https://images.unsplash.com/photo-1581235720704-06d3acfcb36f?q=80&w=2080&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        name: "Ecommerce",
+        desc: "",
+    },
+    {
+        image: "https://images.unsplash.com/photo-1572635196184-84e35138cf62?q=80&w=2080&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        name: "Hotel",
+        desc: "",
+    },
+    {
+        image: "https://images.unsplash.com/photo-1515955656352-a1fa3ffcd111?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+        name: "Building",
+        desc: "",
+    },
+]
+
+//ICONS
+export const iconsList = [
+    {
+        icon: "app-skills/html",
+        name: "HTML",
+    },
+    {
+        icon: "app-skills/css",
+        name: "CSS",
+    },
+    {
+        icon: "app-skills/tailwind",
+        name: "Tailwind",
+    },
+    {
+        icon: "app-skills/javascript",
+        name: "JavaScript",
+    },
+    {
+        icon: "app-skills/typescript",
+        name: "TypeScript",
+    },
+    {
+        icon: "app-skills/react",
+        name: "React.js",
+    },
+    {
+        icon: "app-skills/next",
+        name: "Next.js",
+    },
+    {
+        icon: "app-skills/astro",
+        name: "Astro",
+    },
+    {
+        icon: "app-skills/prisma",
+        name: "Prisma",
+    },
+    //{
+    //    icon: "app-skills/kmp",
+    //    name: "Kotlin (KMP)",
+    //},
+]
+
+/**export const gameIconList = [
+    {
+        icon: "game-skills/affinity-designer",
+        name: "Affinity Designer",
+    },
+    {
+        icon: "game-skills/blender",
+        name: "Blender",
+    },
+    {
+        icon: "game-skills/godot",
+        name: "Godot",
+    },
+    {
+        icon: "game-skills/gdscript",
+        name: "GDScript",
+    },
+    {
+        icon: "game-skills/unreal",
+        name: "Unreal Engine",
+    },
+]**/
